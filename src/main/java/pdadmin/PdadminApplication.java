3a52f4247b1575dd5224e52b7e7d997074362d5c
Constantin <cirimia100@gmail.com>
2023-05-31 10:40:28 -0500

    First commit

---
+++ src/main/java/pdadmin/PdadminApplication.java
@@ -0,0 +1,13 @@
+package pdadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pdadmin/PdadminApplicationTests.java
@@ -0,0 +1,13 @@
+package pdadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
