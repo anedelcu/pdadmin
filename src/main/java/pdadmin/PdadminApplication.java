547fb15c587753ccbee76a6ad7079e7d1bc7366e
Constantin <cirimia100@gmail.com>
2023-05-31 22:22:25 -0500

    Added some comments and some formatting

--- src/main/java/pdadmin/PdadminApplication.java
+++ src/main/java/pdadmin/PdadminApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PdadminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PdadminApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PdadminApplication.class, args);
+    }
 }
