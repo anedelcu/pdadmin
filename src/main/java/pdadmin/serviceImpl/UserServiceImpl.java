d5719f8982c3e9951ce88f7cfcec2dc462247eae
Alin Nedelcu <anedelcu104@gmail.com>
2023-06-02 10:17:25 -0500

    Exception handing added

--- src/main/java/pdadmin/serviceImpl/UserServiceImpl.java
+++ src/main/java/pdadmin/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package pdadmin.serviceImpl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import pdadmin.dao.UserDAO;
@@ -15,7 +17,8 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Resource UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    @Resource private UserRepository userRepository;
     //TODO Add log4j, and add logs.info in the methods, log.warn and log.error
     //Do some research when you have to add each of them
 
@@ -24,25 +27,48 @@ public class UserServiceImpl implements UserService {
     public UserDAO getUserRole(Long userId) {
         UserDAO userDAO = null;
         try {
+            if(userId == null || !isNumeric(userId.toString())) {
+                throw new NumberFormatException("Invalid UserId format");
+            }
             User user = userRepository.findById(userId);
             if (user != null) {
                 String role = user.getRole();
                 userDAO = new UserDAO(userId, role);
             }
         } catch (UserNotFoundException e) {
-            // log.error .... dont print stack trace
-            e.printStackTrace();
+            // log.error .... don't print stack trace
+            logger.error("UserNotFoundException occurred while retrieving user role: {}", e.getMessage());
+        }
+        catch (NumberFormatException e) {
+            logger.error("IllegalArgumentException occurred: {}", e.getMessage());
         }
         return userDAO;
     }
 
+    private boolean isNumeric(String str) {
+        try {
+            Long.parseLong(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     @Override
     public List<User> findAll() {
-        return userRepository.findAll();
+        try{
+            return userRepository.findAll();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error occurred while fetching all users. ", e);
+        }
     }
 
     @Override
     public Long addUser(User user) {
+        if(user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
         User newUser = userRepository.save(user);
         return newUser.getId();
     }
