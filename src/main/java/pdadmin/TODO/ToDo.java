90ddae69723dbbb5eb0fa167826e85c7aa5dba7b
Constantin <cirimia100@gmail.com>
2023-06-02 11:40:07 -0500

    To Do

---
+++ src/main/java/pdadmin/TODO/ToDo.java
@@ -0,0 +1,17 @@
+package pdadmin.TODO;
+
+public class ToDo {
+    /**
+     * This class will be the place where you will get your requirements. Imagine this being like a
+     * JIRA page. You could actually see if you can make an account on Kira and create tasks.
+     *
+     * <p>New Task:
+     *
+     * <p>As a doctor, I should have the ability to log in and import a csv file with multiple
+     * patients details, and those patients should be added into our database. The CSV file will
+     * have the following columns:
+     * First name, Last Name, DOB, Doctor Assigned, Doctor Notes, Diagnostic, Insurance Member Id, Insurance Group Id
+
+     Once the doctor will upload this file, they will be able to see the patients in the database, ONLY if the patient is associated with that respective doctor
+     */
+}
